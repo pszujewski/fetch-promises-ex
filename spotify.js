937587c5f7169d1209a3e0ad23637068271df7c7
Alvin Law <alvinlaw@gmail.com>
2017-03-16 13:34:58 -0700

    to finish

--- spotify.js
+++ spotify.js
@@ -5,6 +5,7 @@ var getFromApi = function(endpoint, query={}) {
         if (!response.ok) {
             return Promise.reject(response.statusText);
         }
+        console.log(response);
         return response.json();
     });
 };
@@ -12,5 +13,25 @@ var getFromApi = function(endpoint, query={}) {
 
 var artist;
 var getArtist = function(name) {
-    // Edit me!
+    const artistQuery = { q: name, limit: 1, type: 'artist' };
+    return getFromApi("search", artistQuery).then(function(item){
+        console.log('The item is: ',item);
+        artist= item.artists.items[0];
+        let artistId= item.artists.items[0].id;
+        console.log('Artist ID is: ', artistId);
+        let relatedArtistUrl= `https://api.spotify.com/v1/artists/${artistId}/related-artists`;
+        console.log(artist);
+
+        return fetch(relatedArtistUrl);
+    }).then(function(response) {
+        return response.json();
+    }).then(function(parsedData){
+        console.log('Parsed Data: ',parsedData);
+        artist.related = parsedData.artists;
+        console.log('Returning: ',artist);
+        return artist;
+    })
+    .catch(function(err){
+        console.error(err);
+    });
 };
